0a3746ece5ffe54add239d7546c0b94552a4d9e2
Nikita Leshenko <nikita@leshenko.net>
2015-05-01 22:21:09 +0300

    Easing

--- pid.js
+++ pid.js
@@ -2,28 +2,50 @@
 
 (function() {
 
-    var prevTime = -16;
-
+    ////////// DISPLAY RELATED //////////
     var c = document.getElementById("c");
     var ctx = c.getContext("2d");
 
     function resizeCanvas() {
         c.width = window.innerWidth;
-        c.height = window.innerHeight;
+        // c.height = window.innerHeight;
     }
     resizeCanvas();
     window.addEventListener("resize", resizeCanvas);
+
+    ////////// PID //////////
+    var prevTime = -16;
+    var x = 20;
+    var setpoint = x;
+
+    function pid(dt) {
+        x += (setpoint - x) * 0.1;
+    }
 
     function update(time) {
         var dt = time - prevTime;
         prevTime = time;
 
-        ctx.fillStyle = "green";
+        pid(dt);
+
+        ctx.fillStyle = "#E7D492";
         ctx.fillRect(0, 0, c.width, c.height);
+
+        ctx.fillStyle = "#7B5747";
+        ctx.beginPath();
+        ctx.arc(x, c.height / 2, 20, 0, 2 * Math.PI, false);
+        ctx.closePath();
+        ctx.fill();
 
         requestAnimationFrame(update);
     }
 
     update(0);
+
+    ////////// CLICKS //////////
+    function canvasClick(e) {
+        setpoint = e.clientX;
+    }
+    c.addEventListener("click", canvasClick);
 
 })();
